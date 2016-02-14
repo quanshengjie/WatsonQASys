15a0310cdb42dd1ac9a5dceab164d583ef3f7ca2
yangyihan <yangyihan@a5267085-b05c-4c94-b217-f2427ebed250>
2016-02-14 00:33:46 +0000

    JAVASCRIPT FOR WEB
    git-svn-id: svn+ssh://theteamtoday.com/usr/local/svn/repos/testProject@21 a5267085-b05c-4c94-b217-f2427ebed250

---
+++ QASys/WebContent/assets/js/ProcessRequestsForSignUp.js
@@ -0,0 +1,107 @@
+var xmlobj;
+var fname;
+var mname;
+var lname;
+var male;
+var female;
+var notknow;
+var email;
+var pwd;
+var confirmpwd;
+var month;
+var day;
+var year;
+var university;
+var major;
+var gpascale;
+var cgpa;
+var mgpa;
+var program;
+
+function CreateXMLHttpRequest()
+{
+	if(window.ActiveXObject)
+	{
+		xmlobj=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	else
+	{
+		xmlobj=new XMLHttpRequest();
+	}
+}
+
+document.getElementById("signup").addEventListener('click',function(event)
+{
+	event.stopPropagation();
+	event.preventDefault();
+	
+	fname=document.getElementById("fname");
+	mname=document.getElementById("mname");
+	lname=document.getElementById("lname");
+	male=document.getElementById("male");
+	female=document.getElementById("female");
+	notknow=document.getElementById("notknow");
+	email=document.getElementById("email");
+	pwd=document.getElementById("pwd");
+	confirmpwd=document.getElementById("confirmpwd");
+	month=document.getElementById("month");
+	day=document.getElementById("day");
+	year=document.getElementById("year");
+	university=document.getElementById("university");
+	major=document.getElementById("major");
+	gpascale=document.getElementById("gpascale");
+	cgpa=document.getElementById("cgpa");
+	mgpa=document.getElementById("mgpa");
+	program=document.getElementById("program");
+
+	CreateXMLHttpRequest();
+	xmlobj.open("POST","../Example2/WebServer",true);
+	xmlobj.onreadystatechange=GetFeedback;
+	xmlobj.setRequestHeader("type","signup");
+	xmlobj.setRequestHeader("fname",fname.value);
+	xmlobj.setRequestHeader("mname",mname.value);
+	xmlobj.setRequestHeader("lname",lname.value);
+	if(male.checked==true)
+	{
+		xmlobj.setRequestHeader("gender","male");
+	}
+	else if(female.checked==true)
+	{
+		xmlobj.setRequestHeader("gender","female");
+	}
+	else
+	{
+		xmlobj.setRequestHeader("gender","notknow");
+	}
+	xmlobj.setRequestHeader("email",email.value);
+	xmlobj.setRequestHeader("pwd",pwd.value);
+	xmlobj.setRequestHeader("confirmpwd",confirmpwd.value);
+	xmlobj.setRequestHeader("month",month.value);
+	xmlobj.setRequestHeader("day",day.value);
+	xmlobj.setRequestHeader("year",year.value);
+	xmlobj.setRequestHeader("university",university.value);
+	xmlobj.setRequestHeader("major",major.value);
+	xmlobj.setRequestHeader("gpascale",gpascale.value);
+	xmlobj.setRequestHeader("cgpa",cgpa.value);
+	xmlobj.setRequestHeader("mgpa",mgpa.value);
+	xmlobj.setRequestHeader("program",program.value);
+	xmlobj.send(null);
+});
+
+function GetFeedback()
+{
+	if(xmlobj.readyState==4 && xmlobj.status==200)
+	{
+		var response=xmlobj.responseText;
+		if(response=="This email address has been registered!")
+		{
+			alter(response);
+		}
+		else
+		{
+			alert(response);
+			var url="index.html?email="+email.value;
+			window.location.assign(url);
+		}
+	}
+}
