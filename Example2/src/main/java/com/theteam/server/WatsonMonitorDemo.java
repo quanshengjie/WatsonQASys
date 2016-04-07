8840693a50c6f2ce37bd26cb1f2b5cc82d036b9d
quanshengjie <quanshengjie@a5267085-b05c-4c94-b217-f2427ebed250>
2016-04-07 17:23:00 +0000

    Add Watson Monitor Demo
    git-svn-id: svn+ssh://theteamtoday.com/usr/local/svn/repos/testProject@107 a5267085-b05c-4c94-b217-f2427ebed250

---
+++ Example2/src/main/java/com/theteam/server/WatsonMonitorDemo.java
@@ -0,0 +1,46 @@
+package com.theteam.server;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WatsonMonitorDemo
+ */
+public class WatsonMonitorDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WatsonMonitorDemo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String type=request.getHeader("type");
+    	if(type != null && type.equals("down"))
+    	{
+    		NotificationMailSender.Instance.simpleSendDown();
+    	}
+    	else if(type != null && type.equals("up"))
+    	{
+    		NotificationMailSender.Instance.simpleSendUp();
+    	}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
